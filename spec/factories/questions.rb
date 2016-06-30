490449a9c970d57dc20248705045601134919931
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-06-30 14:19:27 +0300

    rspec - controllers

---
+++ spec/factories/questions.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :question do
+    sequence(:text) { |n| "В каком году была космичесая одиссея #{n}?" }
+
+    sequence(:level) { |n| n % 15}
+
+    answer1 {"#{rand(2001)}"}
+    answer2 {"#{rand(2001)}"}
+    answer3 {"#{rand(2001)}"}
+    answer4 {"#{rand(2001)}"}
+  end
+end
+
+
+# PS: неплохой фильмец
+# https://ru.wikipedia.org/wiki/Космическая_одиссея_2001_года
+
+
