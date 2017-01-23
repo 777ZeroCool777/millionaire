fc32a15b6bff9516cd90784b2781a8c155281f77
zerocool <exwarvlad@gmail.com>
2017-01-23 21:42:11 +0200

    change host domain for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'khsm.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'test-millioner-test'}
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
